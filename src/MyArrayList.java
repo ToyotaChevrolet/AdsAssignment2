033e0bb70580cb27bc0c1f7fbcdfdfd7c38367e8
Merey Abdikarim <124917188+ToyotaChevrolet@users.noreply.github.com>
2023-04-16 17:43:16 +0600

    Update MyArrayList.java

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,17 +1,116 @@
-import java.util.List;
+public class MyArrayList<T> implements MyList<T> {
 
-public class MyArrayList<T> implements List<T> {
-
-    private Object[] arr;
-    private int length;
+    private Object[] array;
+    private int size = 0;
+    private int capacity = 5;
 
     public MyArrayList() {
-        this(5);
+        array = new Object[capacity];
     }
 
-    public MyArrayList(int initialCapacity) {
-        arr = new Object[initialCapacity];
+    @Override
+    public int size() {
+        return size;
     }
 
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(o)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void add(T newItem) {
+        if (size == capacity) {
+            increaseBuffer();
+        }
+        array[size++] = newItem;
+    }
+
+    @Override
+    public void add(T newItem, int index) {
+        if (index > capacity || index < 0) throw new IndexOutOfBoundsException();
+        if (size == capacity) {
+            increaseBuffer();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = newItem;
+        size++;
+    }
+
+
+    @Override
+    public boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == item) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        T removed = (T) array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+        return removed;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        array = new Object[size];
+    }
+
+    @Override
+    public T get(int index) {
+        return (T) array[index];
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == o) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (array[i] == o) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (((Comparable<T>) array[j]).compareTo((T) array[j + 1]) > 0) {
+                    T temp = (T) array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    private void increaseBuffer() {
+        capacity = (int) (1.5 * capacity);
+        Object[] array2 = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            array2[i] = array[i];
+        }
+        array = array2;
+    }
 
 }
